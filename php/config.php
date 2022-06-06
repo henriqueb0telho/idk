388e730321bb7a9d3817f6d71f0b25fb2eaca2d0
henriqueb0telho <henriquebotelho250604@gmail.com>
2022-06-06 19:12:56 +0000

    idk

--- php/config.php
+++
@@ -1,7 +0,0 @@
-<?php
-    $conn = mysqli_connect("localhost", "root", "", "inventio_chat");
-
-    if(!$conn){
-        echo "Erro ao completar a ligação com o servidor MySQL<br>Resposta do servidor: " . mysqli_connect_error();
-    }
-?>
--- teste.php
+++
@@ -1,5 +0,0 @@
-<?php
-$token = bin2hex(random_bytes(16)); 
-print($token);
-
-?>
